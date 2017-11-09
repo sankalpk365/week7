aec16d7d616658e691d7f908dcaade81825b3a8c
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-11-09 11:15:14 -0500

    sql edit

--- index.php
+++ index.php
@@ -30,13 +30,12 @@ function runQuery($query) {
 $sql = "select * from accounts where id<6 ";
 $results = runQuery($sql);
 if(count($results) > 0)
+
 {
+        echo "No of records:5";
 	echo "<table border=\"1\"><tr><th>Id</th><th>Email</th><th>Firstname</th><th>Lastname</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Password</th></tr>";
-	foreach ($results as $row) {
-	echo
-	"<br>".$row["id"].$row["email"].$row["fname"].$row["lname"].$row["phone"].$row["birthday"].$row["gender"].$row["password"]."<br>";
-		
-	echo "<br>"."<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
+	foreach ($results as $row) {		
+	echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
 	}
 	}
 	else{
